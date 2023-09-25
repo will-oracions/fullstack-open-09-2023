7dc0c7969e7b9bf1d78bc9408ae85d875aae53a5
will-oracions <oracions.dev@gmail.com>
2023-09-25 15:46:35 +0100

    part1.a: done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -37,19 +37,21 @@ const Content = ({
   exercises3,
 }) => (
   <>
-    <p>
-      {part1} {exercises1}
-    </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p>
+    <Part part={part1} exercises={exercises1} />
+    <Part part={part2} exercises={exercises2} />
+    <Part part={part3} exercises={exercises3} />
   </>
 );
-const Total = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => (
+const Total = ({ exercises1, exercises2, exercises3 }) => (
   <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
 );
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
 
 export default App;
