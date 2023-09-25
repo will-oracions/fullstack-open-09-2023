b87d0a2d97102e0e4124e8347e246aeea330d168
will-oracions <oracions.dev@gmail.com>
2023-09-25 15:38:26 +0100

    execise 1.1: done

--- part1/app1/src/App.jsx
+++ part1/app1/src/App.jsx
@@ -9,19 +9,47 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   );
 };
+
+const Header = ({ course }) => <h1>{course}</h1>;
+const Content = ({
+  part1,
+  part2,
+  part3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) => (
+  <>
+    <p>
+      {part1} {exercises1}
+    </p>
+    <p>
+      {part2} {exercises2}
+    </p>
+    <p>
+      {part3} {exercises3}
+    </p>
+  </>
+);
+const Total = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => (
+  <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+);
 
 export default App;
